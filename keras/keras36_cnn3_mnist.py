6654f7f3abd77b9d85617677eb5ca91deb47f843
wendyscode <glgl1104@naver.com>
2026-09-16 10:47:52 +0900

    update

--- keras/keras36_cnn3_mnist.py
+++ keras/keras36_cnn3_mnist.py
@@ -5,6 +5,8 @@ import numpy as np
 from tensorflow.keras.datasets import mnist
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Conv2D, Dense,Dropout,Flatten
+import time
+from sklearn.metrics import accuracy_score
 
 #1. 데이터
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
@@ -52,14 +54,42 @@ model.add(Dropout(0.2))
 model.add(Conv2D(32,(2,2,), activation='relu')) #(21,21,32)
 model.add(Dropout(0.2))
 model.add(Conv2D(16,(2,2,), activation='relu')) #(20,20,16)
+model.add(Flatten())                        #납작하게 펴기📦➡️📏
 
-model.add(Flatten())
-model.add(Dense(units=32,activation='relu'))
+model.add(Dense(units=32,activation='relu')) #👉 뉴런 32개를 만들겠다
 model.add(Dropout(0.2))
 model.add(Dense(units=16, input_shape =(32,),activation='relu'))
-
 model.add(Dense(10,activation='softmax'))   #(10,)
 model.summary()
+
+#3. 컴파일 , 훈련
+model.compile(loss='categorical_crossentropy', optimizer = 'adam',
+            metrics = ['acc'],
+            )   
+
+start_time = time.time()
+model.fit(x_train,y_train, epochs=50, batch_size=128,
+        verbose = 1, 
+        validation_split = 0.2,
+          )
+end_time = time.time()
+
+#4. 평가예측
+print("==============model.evaluate======================")
+loss = model.evaluate(x_test, y_test, verbose=1)
+print('loss :', loss[0])
+print('loss :', loss[1])
+
+y_predict = model.predict(x_test)
+
+y_predict = np.argmax(y_predict,axis=1,).reshape(-1,1)
+y_test = np.argmax(y_test,axis=1,).reshape(-1,1)
+
+acc_score = accuracy_score(y_test, y_predict)
+print('accuracy_score : ', acc_score)
+print('걸린시간 : ', round(end_time-start_time,2), '초')
+
+
 
 
 
